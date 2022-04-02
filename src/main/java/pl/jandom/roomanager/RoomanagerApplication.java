b2891c711f4cb6756ef2682f77056aaa29f77fe5
janluk00 <mrjankosz1@gmail.com>
2022-04-02 21:38:22 +0200

    adds spring framework

---
+++ src/main/java/pl/jandom/roomanager/RoomanagerApplication.java
@@ -0,0 +1,13 @@
+package pl.jandom.roomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jandom/roomanager/RoomanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jandom.roomanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
